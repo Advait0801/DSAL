c9be4c12d34134fd743fd900a5cb878c8ee2aa72
Advait Naik <ad.naik0801@gmail.com>
2023-02-23 22:29:55 +0530

    Initial Commit

--- Assignment_2/main.cpp
+++ Assignment_2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Queue.cpp"
+#include "Stack.cpp"
 using namespace std;
 
 class Node{
@@ -23,6 +24,10 @@ class BT{
 public:
     BT(){
         this -> root = nullptr;
+    }
+
+    Node *getroot(){
+        return this -> root;
     }
 
     void insert(int item){
@@ -54,20 +59,106 @@ public:
                 }
             }
         }
+    }    
+
+    void displayInorder(){
+        Stack <Node *> s;
+        Node *current = this -> root;
+
+        while(!s.isEmpty() || current!=nullptr){
+            //Reach the left most node of the current node
+            //Push each left node into the stack
+            while(current!=nullptr){
+                s.push(current);
+                current = current -> left;
+            }
+
+            //current is null so pop the stack and print the data
+            current = s.gettop();
+            s.pop();
+
+            cout<<current -> data<<" ";
+
+            //now check the right part
+            current = current -> right;
+        }
     }
 
-    Node *getroot(){
-        return this -> root;
+    void displayPreorder(){
+        Stack <Node *> s;
+        s.push(this -> root);
+        Node *current = nullptr;
+
+        while(!s.isEmpty()){
+            current = s.gettop();
+            cout<<current -> data<<" ";
+            s.pop();
+
+            //now push right child first and then left so that pop time, left comes first 
+            if(current -> right != nullptr){
+                s.push(current -> right);
+            }
+            if(current -> left != nullptr){
+                s.push(current -> left);
+            }
+        }
     }
 
-    void displayPreorder(Node *node){
+    void displayPostorder(){
+        Stack <Node *> s1,s2;
+        s1.push(this -> root);
+        Node *current = nullptr;
+
+        while(!s1.isEmpty()){
+            //pop the top element of first stack and push it into the second stack
+            current = s1.gettop();
+            s2.push(current);
+            s1.pop();
+
+            //now push left child first into s1 and then right so while pop it will go in postorder
+            if(current -> left != nullptr){
+                s1.push(current -> left);
+            }
+            if(current -> right != nullptr){
+                s1.push(current -> right);
+            }
+        }
+
+        //now all the nodes of the tree are traversed
+        //now the stack s2 is in postorder reversed
+        //so pop and display each element
+        
+        while(!s2.isEmpty()){
+            current = s2.gettop();
+            cout<<current -> data<<" ";
+            s2.pop();
+        }
+    }
+
+    void displayPostorder(Node *node){
         if(node == nullptr){
             return;
         }
-
+        
+        displayPostorder(node -> left);
+        displayPostorder(node -> right);
         cout<<node -> data<<" ";
-        displayPreorder(node -> left);
-        displayPreorder(node -> right);
+    }
+    int height(Node* node){
+        if(root == nullptr){
+            return -1;
+        }
+        int left = height(node->left);
+        int right = height(node->right);
+        int count;
+
+        if(left>=right){
+            count = left + 1;
+        }
+        else{
+            count = right + 1;
+        }
+        return count;
     }
 
 };
@@ -82,7 +173,9 @@ int main(){
     b.insert(12);
 
     Node *root = b.getroot();
-    b.displayPreorder(root);
+    b.displayPostorder(root);
+    cout<<endl;
+    b.displayPostorder();
 
     return 0;
 }
