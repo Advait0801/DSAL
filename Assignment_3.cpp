5009f2a618166be52630dd9b06c901f7c1debd4a
Advait Naik <ad.naik0801@gmail.com>
2023-02-28 22:06:11 +0530

    Initial Commit

--- Assignment_3.cpp
+++ Assignment_3.cpp
@@ -1,7 +1,7 @@
 /*
-Create an inordered threaded binary search tree. Perform inorder, preorder 
-traversals without recursion and deletion of a node. Analyze time and space 
-complexity of the algorithm.
+Create an inordered threaded binary search tree. Perform inorder, preorder
+traversals without recursion and deletion of a node. Analyze time and
+space complexity of the algorithm.
 
 Name : Advait Naik
 Roll No : 21354
@@ -16,16 +16,52 @@ class Node{
     bool rightThread;
 
 public:
-    friend class TBT;
+    Node(int i){
+        this -> data = i;
+        this -> left = nullptr;
+        this -> right = nullptr;
+        this -> rightThread = false;
+    }
+
+    friend class TBST;
 };
 
-class TBT{
+class TBST{
     Node *root;
 
 public:
-    TBT(){
-        this -> root = nullptr;
+    TBST(int i){
+        this -> root = new Node(i);
     }
 
-    
+    void insert(int i){
+        Node *current = root;
+        Node *previous = nullptr;
+
+        while(current!=nullptr){
+            previous = current;
+            if(i < current -> data){
+                current = current -> left;
+            }
+            else{
+                current = current -> right;
+            }
+        }
+
+        if(i < previous -> data){
+            Node *newnode = new Node(i);
+            previous -> left = newnode;
+            newnode -> right = previous;
+            newnode -> rightThread = true;
+        }
+        else{
+            Node *newnode = new Node(i);
+            newnode -> right = previous -> right;
+            newnode -> rightThread = previous -> rightThread;
+            previous -> rightThread = false;
+            previous -> right = newnode;
+        }
+    }
+
+
 };
