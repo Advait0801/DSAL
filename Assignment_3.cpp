cc6d3f7192ee789c96a9f9cd79122b3a9d84c6b2
Advait Naik <ad.naik0801@gmail.com>
2023-03-13 22:35:32 +0530

    Final Commit

--- Assignment_3.cpp
+++ Assignment_3.cpp
@@ -38,7 +38,7 @@ public:
     
     //inserts a node as per insertion in BST
     void insert(int i){
-        Node *current = root;
+        Node *current = this -> root;
         Node *previous = nullptr;
 
         while(current!=nullptr){
@@ -161,13 +161,93 @@ public:
             return node;
         }
     }
-
-    Node *inorderPredecessor(Node *node){
-        
-    }
     
     void deleteNode(int key){
+        Node *current = this -> root;
+	    Node *previous = nullptr;
+	    while( true ){
+		    if( current -> left == nullptr && current -> rightThread ) {
+		    	break;
+		    }
+		    if (current -> data > key) {
+			    // Go left
+		    	previous = current;
+			    current = current -> left;
+		    }
+		    else if( current -> data < key ) {
+			    // Go right if rightThread is False
+		    	// We wish to travel down the tree,
+		    	// if we encounter a thread, break the loop (as we've encountered a leaf node)
+			    if (!(current -> rightThread)) {
+				    previous = current;
+				    current = current->right;
+			    }
+			    else {
+				    break;
+			    }
+		    }
+		    else {
+			    break ;
+		    }
+	    }
 
+	    cout << "data: " << current -> data << "\n" ;
+	    cout << "rightThread: " << current -> rightThread << "\n" ; 
+
+	    cout << "prev data: " << previous -> data << "\n" ;
+	    cout << "prev rightThread: " << previous -> rightThread << "\n" ; 
+ 
+
+	    if( current -> left == nullptr  && (current -> rightThread) ) {
+		    cout << "Leaf Node deletion" << "\n" ; 
+		    // Leaf node deletion
+		    if( previous -> left == current ) {
+		    	cout << "Left child delete" << "\n" ; 
+		    	previous -> left = nullptr ;
+	    	}
+		    else if( previous -> right == current ) {
+			    previous -> rightThread = true ;
+			    previous -> right = current -> right ;
+		    }
+		    delete current ;
+	    }
+	    else if( current -> left != nullptr && current -> right != nullptr && !(current -> rightThread) ) {
+		    cout << "Two children deletion" << "\n" ; 
+		    // current has two children
+		    Node* minInRightTree = current -> right ; 
+		    Node* newParent = current ; 
+		    while( minInRightTree -> left != nullptr ) {
+			    newParent = minInRightTree ;
+			    minInRightTree = minInRightTree -> left ; 
+		    }
+		    int temp = minInRightTree -> data ; 
+		    minInRightTree -> data = current -> data ; 
+		    current -> data = temp ; 
+
+		    previous = current ; 
+		    current = minInRightTree ; 
+		
+		    if( current -> left != nullptr ) {
+			    current -> left -> right = previous ; 
+			    previous -> left = current -> left ; 
+		    }
+		    else if( current -> right != nullptr ) {
+			    previous -> right = current -> right ; 
+		    }
+
+		    delete minInRightTree ; 
+	    }
+	    else {
+		    cout << "Single Child deletion" << "\n" ; 
+		    if( current -> left != nullptr ) {
+		    	current -> left -> right = previous ; 
+			    previous -> left = current -> left ; 
+		    }
+		    else if( current -> right != nullptr ) {
+			    previous -> right = current -> right ; 
+		    }
+		    delete current ;
+	    }
     }
 };
 
@@ -178,7 +258,8 @@ int main(){
     t.insert(35);
     t.insert(67);
     t.insert(43);
-    t.preorderTraversal();
+    t.deleteNode(12);
+    t.inorderTraversal();
 
     return 0;
 }
